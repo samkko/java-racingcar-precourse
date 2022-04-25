d4eff2c35f625a0634a51d031c31d8ac0e432f5d
hskim <wkvk1234@gmail.com>
2022-04-25 11:55:11 +0900

    refactor : remove duplicate code

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,10 +8,6 @@ import racingcar.model.RacingCars;
 import racingcar.view.ConsoleOutputView;
 import racingcar.view.NextStepInputView;
 
-import java.util.concurrent.Callable;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 public class Application {
 
     private static final ViewController viewController = new ViewController(new NextStepInputView(), new ConsoleOutputView());
@@ -19,40 +15,24 @@ public class Application {
 
     public static void main(String[] args) {
 
-        RacingCars racingCars = null;
-        GameCount gameCount = null;
-
-        while(!racingCarInputSuccess());
-        while(!gameCountInputSuccess());
+        while(validateInput(() -> gameController.initRacingCars(viewController.getCarNames())));
+        while(validateInput(() -> gameController.initGameCount(viewController.getGameCount())));
         gameController.play();
         viewController.printMessageWithNewLine("실행 결과");
         viewController.printMessage(gameController.getGameResult());
         viewController.printMessageWithNewLine("최종 우승자: " + gameController.getWinners());
     }
 
-    private static boolean racingCarInputSuccess() {
+    private static boolean validateInput(Runnable runnable) {
         try {
-            gameController.initRacingCars(viewController.getCarNames());
+            runnable.run();
         } catch (IllegalArgumentException e) {
             if(e instanceof MessageException) {
                 viewController.printMessageWithNewLine(((MessageException) e).getFormattedErrorMessage());
-                return false;
+                return true;
             }
         }
 
-        return true;
-    }
-
-    private static boolean gameCountInputSuccess() {
-        try {
-            gameController.initGameCount(viewController.getGameCount());
-        } catch (IllegalArgumentException e) {
-            if(e instanceof MessageException) {
-                viewController.printMessageWithNewLine(((MessageException) e).getFormattedErrorMessage());
-                return false;
-            }
-        }
-
-        return true;
+        return false;
     }
 }
